42a3d9f2d6937bd00c0242368732186bdc79057b
unknown <170219a7@gmail.com>
2024-07-31 20:13:12 +0200

    Update code

--- main.py
+++ main.py
@@ -2,42 +2,45 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-# URL страницы, которую мы будем скрапить
+# URL страницы, которую будем скрапить
 url = "https://ww16.0123movie.net/list/movies.html"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/58.0.3029.110 Safari/537.3"
 }
 
 # Отправка HTTP-запроса к странице
 response = requests.get(url, headers=headers)
 
-# Take content
+# Получение содержимого страницы
 html_content = response.text
 
 if response.status_code == 200:
-    # Parsing HTML code with BeautifulSoup
+    # Парсинг HTML-кода с помощью BeautifulSoup
     soup = BeautifulSoup(html_content, "html.parser")
 
     movie_cards = soup.find_all("div", class_="card h-100 border-0 shadow", limit=20)
 
-    # List to hold movie data
+    # Список для хранения данных о фильмах
     movies = []
 
     for card in movie_cards:
-        #Name
+        # Название
         title_tag = card.find("h2", class_="card-title text-light fs-6 m-0")
         title = title_tag.text.strip() if title_tag else "No Name"
-        #Link
+        # Ссылка
         link_tag = card.find("a", class_="rounded poster")
         link = link_tag["href"] if link_tag else "No Link"
 
         movies.append({"Title": title, "Link": link})
 
-    # Create a DataFrame from the list of dictionaries
+    # Создание DataFrame из списка словарей
     df = pd.DataFrame(movies)
 
-    # Save the DataFrame to an Excel file
-    df.to_excel("movies.xlsx", index=False)
+    # Сохранение DataFrame в файл Excel
+    df.to_excel("Excel/movies.xlsx", index=False)
+
+    # Сохранение DataFrame в CSV файл
+    df.to_csv("csv/movies.csv", index=False)
 
 else:
-    print(f"Problem with access: {response.status_code}")
+    print(f"Проблема с доступом: {response.status_code}")
